a77d976aaa9b26b138ae1227457e550afb1d6cae
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-06-15 11:34:17 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -610,31 +610,30 @@ def main():
             LOGGER.warning(e.message)
 
 
-    test_handler = CommandHandler("test", test)
+    test_handler = CommandHandler("t
 
 
-    help_handler = CommandHandler("help", get_help)
-    help_callback_handler = CallbackQueryHandler(help_button, pattern=r"help_.*")
 
-    settings_handler = CommandHandler("settings", get_settings)
-    settings_callback_handler = CallbackQueryHandler(settings_button, pattern=r"stngs_")
 
-    about_callback_handler = CallbackQueryHandler(Masha_about_callback, pattern=r"masha_")
-    source_callback_handler = CallbackQueryHandler(Source_about_callback, pattern=r"source_")
 
-    donate_handler = CommandHandler("donate", donate)
-    migrate_handler = MessageHandler(Filters.status_update.migrate, migrate_chats)
+   
+
+
+
+
+
+
+   
 
     # dispatcher.add_handler(test_handler)
 
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(about_callback_handler)
-    dispatcher.add_handler(source_callback_handler)
-    dispatcher.add_handler(settings_handler)
-    dispatcher.add_handler(help_callback_handler)
-    dispatcher.add_handler(settings_callback_handler)
-    dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
+
+
+
+
+
+
+ 
 
     dispatcher.add_error_handler(error_callback)
 
