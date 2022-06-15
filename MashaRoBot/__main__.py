733ed7870417efdaca0a82c83699989dbe856d6b
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-06-15 13:53:33 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -627,15 +627,7 @@ def main():
 
     # dispatcher.add_handler(test_handler)
 
-    dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(about_callback_handler)
-    dispatcher.add_handler(source_callback_handler)
-    dispatcher.add_handler(settings_handler)
-    dispatcher.add_handler(help_callback_handler)
-    dispatcher.add_handler(settings_callback_handler)
-    dispatcher.add_handler(migrate_handler)
-    dispatcher.add_handler(donate_handler)
-
+    
     dispatcher.add_error_handler(error_callback)
 
     if WEBHOOK:
